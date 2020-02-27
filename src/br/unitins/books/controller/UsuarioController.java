f853f480b05d471f5dc11986333459642c81dbd0
IannBraga <bragaprestes@gmail.com>
2020-02-27 11:27:31 -0300

    deu bom

--- src/br/unitins/books/controller/UsuarioController.java
+++ src/br/unitins/books/controller/UsuarioController.java
@@ -1,10 +1,60 @@
 package br.unitins.books.controller;
 
-import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
-@Named
-@RequestScoped
-public class LoginController {
+import br.unitins.books.model.Usuario;
 
+@Named
+@ViewScoped
+public class UsuarioController implements Serializable {
+
+	/**
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Usuario usuario;
+	private List<Usuario> listaUsuario;
+
+	public void incluir() {
+		getListaUsuario().add(getUsuario());
+		limpar();
+	}
+	
+	public void editar(Usuario usuario) {
+		setUsuario(usuario);
+	}
+	
+	public void alterar() {
+		
+	}
+	
+	public void remover() {
+		getListaUsuario().remove(getUsuario());
+		limpar();
+	}
+	
+	public void limpar() {
+		usuario = null;
+	}
+	
+	public List<Usuario> getListaUsuario() {
+		if(listaUsuario == null)
+			listaUsuario = new ArrayList<Usuario>();
+		return listaUsuario;
+	}
+	
+	public Usuario getUsuario() {
+		if(usuario == null)
+			usuario = new Usuario();
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 }
---
+++ src/br/unitins/books/model/Usuario.java
@@ -0,0 +1,50 @@
+package br.unitins.books.model;
+
+public class Usuario implements Cloneable {
+	
+	private Integer id;
+	private String Nome;
+	private String login;
+	private String senha;
+	private String email;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+	
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
